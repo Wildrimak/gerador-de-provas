5e6b9c38f3c7a06c055988c5131af78f5316ed55
Wildrimak <wildrimak@gmail.com>
2025-01-17 20:26:53 +0000

    Ignora mapeamento de id

--- questions/src/main/java/br/com/wildrimak/questions/api/mappers/QuestaoMapper.java
+++ questions/src/main/java/br/com/wildrimak/questions/api/mappers/QuestaoMapper.java
@@ -4,6 +4,7 @@ import br.com.wildrimak.questions.api.dtos.QuestaoRequest;
 import br.com.wildrimak.questions.api.dtos.QuestaoResponse;
 import br.com.wildrimak.questions.dominio.model.Questao;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +12,7 @@ public interface QuestaoMapper {
 
     QuestaoMapper INSTANCE = Mappers.getMapper(QuestaoMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     Questao toQuestao(QuestaoRequest questaoRequest);
 
     QuestaoResponse fromQuestao(Questao questao);
