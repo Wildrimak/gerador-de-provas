3285ba8a2d6f2170b80f1f493cc1aab76b9a44bf
Wildrimak <wildrimak@gmail.com>
2025-01-19 18:51:37 -0300

    Plano de desenvolvimento criado

--- questions/src/main/java/br/com/wildrimak/questions/QuestionsApplication.java
+++ questions/src/main/java/br/com/wildrimak/questions/QuestionsApplication.java
@@ -8,6 +8,27 @@ public class QuestionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionsApplication.class, args);
+		System.out.println(proximosPassos());
+	}
+
+	private static String proximosPassos() {
+		String start = "\n\n\n\n\n";
+		final int finalizados = 5;
+		int i = 1;
+		start += i++ + ". Finalizar modelagem dos modelos de negócio\n";
+		start += i++ + ". Terminar mapeamento entre models de negocio e de jpa\n";
+		start += i++ + ". Finalizar modelagem dos dtos das controllers\n";
+		start += i++ + ". Terminar mapeamento entre models de negocio e os dtos da controller\n";
+		start += i++ + ". Atualizar endpoint de criar questao para lista de questoes\n";
+		start += i++ + ". Criar endpoint que permite filtras questoes com base em um tema\n";
+		start += "\n\n";
+		int total = finalizados + i;
+		double percentual = (100 * finalizados) / (double) total;
+		String resultado = String.format("%.2f", percentual) + "%";
+		String line = "+-------------------------------+\n";
+		String show = "| CONCLUÍDO " + resultado + " das tarefas! |\n";
+		start += line + show + line;
+		return start;
 	}
 
 }
