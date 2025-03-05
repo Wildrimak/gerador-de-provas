9854cd6912e8c8790d6a97a1e767cca1101917eb
Wildrimak <wildrimak@gmail.com>
2025-03-05 03:54:45 +0000

    Adiciona compotamento do cadastra questoes

--- questions-frontend/src/app/routes.ts
+++ questions-frontend/src/app/routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastraQuestoesComponent } from './cadastra-questoes/cadastra-questoes.component'
 import { GeraProvaComponent } from './gera-prova/gera-prova.component'
+import { ProcessaQuestoesComponent } from './processa-questoes/processa-questoes.component';
+import { VisualizaQuestoesComponent } from './visualiza-questoes/visualiza-questoes.component';
 
 const routeConfig: Routes = [
     {
@@ -18,6 +20,16 @@ const routeConfig: Routes = [
         path: 'gera-prova',
         component: GeraProvaComponent,
         title: 'Gera Prova',
+    },
+    {
+        path: 'processa-questoes',
+        component: ProcessaQuestoesComponent,
+        title: 'Processa Questoes',
+    },
+    {
+        path: 'visualiza-questoes',
+        component: VisualizaQuestoesComponent,
+        title: 'Visualiza Questoes',
     },
     {
         path: '**',
