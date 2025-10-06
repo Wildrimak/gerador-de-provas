594fb78ac5c96f97373dcb379f8c386092315a03
Wildrimak <wildrimak@gmail.com>
2025-10-06 23:56:32 +0000

    Ajustes na descricao do projeto e nas aplicacoes

--- questions/src/main/java/br/com/wildrimak/questions/QuestionsApplication.java
+++ questions/src/main/java/br/com/wildrimak/questions/QuestionsApplication.java
@@ -8,22 +8,6 @@ public class QuestionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionsApplication.class, args);
-		System.out.println(proximosPassos());
-	}
-
-	private static String proximosPassos() {
-		String start = "\n\n\n\n\n";
-		final int finalizados = 11;
-		int i = 1;
-		start += i++ + ". Refatorar a parte de service para que o dominio de negocios, nao tenha dependencias com o dominio de data\n";
-		start += "\n\n";
-		int total = finalizados + i;
-		double percentual = (100 * finalizados) / (double) total;
-		String resultado = String.format("%.2f", percentual) + "%";
-		String line = "+-------------------------------+\n";
-		String show = "| CONCLUÍDO " + resultado + " das tarefas! |\n";
-		start += line + show + line;
-		return start;
 	}
 
 }
