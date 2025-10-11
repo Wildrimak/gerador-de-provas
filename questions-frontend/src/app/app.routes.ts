ac6c400ba3ed7f7393c32f9569d4a0ca46142f97
Wildrimak <wildrimak@gmail.com>
2025-10-11 16:10:14 +0000

    Troca a pagina inicial pelo componente de gerencia questoes

--- questions-frontend/src/app/app.routes.ts
+++ questions-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastraQuestoesComponent } from './cadastra-questoes/cadastra-questoes.component';
 import { GeraProvaComponent } from './gera-prova/gera-prova.component';
+import { GerenciaQuestoesComponent } from './gerencia-questoes/gerencia-questoes.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'Início' },
+    { path: '', component: GerenciaQuestoesComponent, title: 'Gerência de Questões' },
+    { path: 'home', component: HomeComponent, title: 'Início' },
     { path: 'cadastra-questoes', component: CadastraQuestoesComponent, title: 'Cadastra Questões' },
     { path: 'gera-prova', component: GeraProvaComponent, title: 'Gera Prova' },
     { path: '**', redirectTo: '', pathMatch: 'full' }
