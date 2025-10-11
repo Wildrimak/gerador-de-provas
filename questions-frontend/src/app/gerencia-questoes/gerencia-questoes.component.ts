ad03c578b90893b2ac278db90515d417b297ccbd
Wildrimak <wildrimak@gmail.com>
2025-10-11 02:27:57 +0000

    Adiciona componente gerencia questoes

---
+++ questions-frontend/src/app/gerencia-questoes/gerencia-questoes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerencia-questoes',
+  imports: [],
+  templateUrl: './gerencia-questoes.component.html',
+  styleUrl: './gerencia-questoes.component.css'
+})
+export class GerenciaQuestoesComponent {
+
+}
