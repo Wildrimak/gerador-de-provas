20353de3dc472b65fb77eda7bdc5b64a1ad8d1ff
Wildrimak <finalwildrimak@gmail.com>
2025-10-19 03:12:06 -0300

    Adiciona navbar principal da aplicacao e modifica configuracoes para fazer uso de daisyui + tailwind

---
+++ super-mentores-frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+    ],
+  },
+};
